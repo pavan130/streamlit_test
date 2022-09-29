d5627abe4081d061412d0f9a053e8610455be516
R PAVAN KUMAR <pavan13054@gmail.com>
2022-09-29 13:42:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,3 +108,7 @@ font="monospace"
 
 number = st.sidebar.slider('Select a number:', 0, 10, 5)
 st.write('Selected number from slider widget is:', number)
+
+st.title('st.secrets')
+
+st.write(st.secrets['message'])
