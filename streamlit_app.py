a2a6b40b8635a48d06c8437222dc04dead82b7b2
R PAVAN KUMAR <pavan13054@gmail.com>
2022-09-29 13:19:31 +0530

    day 13

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from datetime import time, datetime
 import pandas as pd
 import numpy as np
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 
 st.header('st.slider')
 
@@ -81,3 +83,10 @@ if coffee:
 
 if cola:
      st.write("Here you go 🥤")
+
+st.header('`streamlit_pandas_profiling`')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+pr = df.profile_report()
+st_profile_report(pr)
